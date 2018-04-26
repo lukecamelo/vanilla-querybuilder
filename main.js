e16fd15234d9f7d86bbfd7a3d544052c587321e7
luke <azathothbeats@gmail.com>
2018-04-26 19:03:20 -0400

    Basic integer comparison is functional

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ const first = document.querySelector('#first').textContent
 const second = document.querySelector('#second').textContent
 const check = document.querySelector('#check')
 
-const option = document.querySelector('#option')
-let selected = option.options[option.selectedIndex].value;
-
 const button = document.querySelector('#activate')
+const option = document.querySelector('#option')
+let selected = option.options[option.selectedIndex].value
 
 console.log(first, second, selected)
 
 button.addEventListener('click', function() {
+  selected = option.options[option.selectedIndex].value
   compare(first, second)
   console.log(selected)
 })
 
 function compare(x, y) {
-  if (selected = 'less') {
-    compareLesser(x, y)
-  } else if (selected = 'greater') {
+  if (selected == 'greater') {
     compareGreater(x, y)
+  } else if (selected == 'less') {
+    compareLesser(x, y)
   }
 }
 
@@ -26,7 +26,7 @@ function compareGreater(x, y) {
   if (x > y) {
     check.textContent = 'greater'
   } else {
-    console.log('error: y greater than x, or some other shit ???')
+    check.textContent = 'Invalid: ' + x + ' is less than ' + y
   }
 }
 
@@ -34,6 +34,6 @@ function compareLesser(x, y) {
   if (x < y) {
     check.textContent = 'lesser'
   } else {
-    console.log('error: x greater than y, or some other shit ??? x2')
+    check.textContent = 'Invalid: ' + x + ' is greater than ' + y
   }
 }
